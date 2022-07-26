313f1b0d32b6c43fd52692d27bc206735fbb1032
Zax71 <zabax32@gmail.com>
2022-07-26 16:32:19 +0100

    Config works

--- src/main.py
+++ src/main.py
@@ -2,13 +2,30 @@
 Endercube-bot by Zax71
 Github: https://github.com/zax71/endercube-bot
 """
-import discord, termcolor
-from dotenv import load_dotenv
+import discord, termcolor, yaml
 
+def loadConfig():
+    configFile = ""
+
+    try:
+        with open('config.yml', "r") as f:
+            configFile = yaml.load(f, Loader=yaml.FullLoader)
+    except:
+        defaultConfig = open('defaultConfig.yml', "r")
+        f = open('config.yml', 'w')
+        f.write(defaultConfig.read())
+    finally:
+        with open('config.yml', "r") as f:
+            configFile = yaml.load(f, Loader=yaml.FullLoader)
+    
+    return configFile
+
+
+config = loadConfig()
 bot = discord.Bot()
 
 @bot.event
 async def on_ready():
     print("Logged in as", bot.user)
 
-bot.run()
+bot.run(config["bot_token"])
